876081532a1cb6bc492417bab5fc7030b0a6206d
DKorablin2 <danila.korablin@unity3d.com>
2024-12-09 02:58:19 +0300

    Update to SAL.Flatbed 1.2.10

--- SAL.Windows/Properties/AssemblyInfo.cs
+++ SAL.Windows/Properties/AssemblyInfo.cs
@@ -1,21 +1,22 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: Guid("c276bad5-c579-4bac-8205-28e2bfe0926e")]
 [assembly: ComVisible(false)]
+[assembly: Guid("c276bad5-c579-4bac-8205-28e2bfe0926e")]
 [assembly: System.CLSCompliant(true)]
 
 #if NETSTANDARD || NETCOREAPP
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=76")]
 #else
+
 [assembly: AssemblyTitle("SAL.Windows")]
-[assembly: AssemblyProduct("SAL Interface for Windows applications .NET4-")]
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2009-2024")]
+[assembly: AssemblyDescription("SAL Interface for Windows applications .NET4-")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
 [assembly: AssemblyConfiguration("Release")]
 #endif
-//[assembly: AssemblyVersion("1.2.9.2")]
+[assembly: AssemblyCompany("Danila Korablin")]
+[assembly: AssemblyProduct("SAL Interface for Windows applications")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2009-2024")]
 #endif
